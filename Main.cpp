4e2cbebbc63eedf4e6da2400935080570ee7978e
HamzehAmiri2020 <HamzehAmiri2020@yahoo.com>
2020-04-12 15:34:00 +0430

    Start MongoDB

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+
+int main(int, char**) {
+	mongocxx::instance instance{};
+}
